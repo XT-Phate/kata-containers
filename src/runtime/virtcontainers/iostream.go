e1d4ef549d60ee2ec05a45cf483b6ad45f705f00
Maxime Bertin <mbertin@luccasoftware.com>
2025-09-11 12:10:14 +0200

    dbg

--- src/runtime/virtcontainers/iostream.go
+++ src/runtime/virtcontainers/iostream.go
@@ -9,8 +9,6 @@ import (
 	"context"
 	"errors"
 	"io"
-
-	"github.com/sirupsen/logrus"
 )
 
 type iostream struct {
@@ -57,7 +55,6 @@ func (s *iostream) stderr() io.Reader {
 }
 
 func (s *stdinStream) Write(data []byte) (n int, err error) {
-	s.container.Logger().Logger.SetLevel(logrus.WarnLevel)
 	if s.closed {
 		s.container.Logger().Error("STDIN CLOSED")
 		return 0, errors.New("stream closed")
